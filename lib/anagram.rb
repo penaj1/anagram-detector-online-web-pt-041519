006e8ff409db0adc9199f1aaeb938d08a870f348
Janice Pena <jipena94@gmail.com>
2019-05-18 18:47:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(find_anagram)
     find_anagram.find_all do |word|
-      if word.split("").sort == self.
+      if word.split("").sort == self.word.split
   end
 
 end
